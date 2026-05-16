353f038c7812d186e2e38bd90f556da69ad910ff
axeldupuis111 <axel.dupuis111@gmail.com>
2026-05-16 17:52:44 +0200

    fix(pips): remove unused import in test file

--- lib/pips.test.ts
+++ lib/pips.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from "vitest";
-import { calculatePips, detectAssetType, getPipValue, getTradeResult } from "./pips";
+import { calculatePips, detectAssetType, getTradeResult } from "./pips";
 
 describe("detectAssetType", () => {
   it("detects XAUUSD variants", () => {
